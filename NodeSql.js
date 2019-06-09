a4b92f3b6bab6351da63e8f2cbb3d3d07192e8ee
johnv <kiyoto_zy@hotmail.com>
2019-06-10 00:43:22 +0800

    update bodyparser

--- NodeSql.js
+++ NodeSql.js
@@ -13,8 +13,16 @@ var conn = mysql.createConnection({
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html')
+    console.log(__dirname)
     res.end('render success')
 })
+
+var app = express()
+var publicDir = (__dirname + '/public/')
+app.use(express.static(publicDir))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
 server.listen('3001', '127.0.0.1', () => {
     console.log('Server is running..')
 })
